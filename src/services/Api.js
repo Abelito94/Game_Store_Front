c146758417ad376cecd1761fee9c580a9512a92a
Abel Esquivel <abelesquivel94@gmail.com>
2020-04-10 16:59:18 +0100

    frontendLast

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gamestore123.herokuapp.com/api",
+  baseURL: process.env.VUE_APP_API_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
